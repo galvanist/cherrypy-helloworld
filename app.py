76505d8e9a5d2a5043d75fcd0d46a3f25796e6a6
galvanist <gleake@gmail.com>
2012-06-12 00:17:33 +0200

    Cherrypy on heroku loopback check bug workaround, tidy config update section and added "hello Heroku" example

--- app.py
+++ app.py
@@ -1,10 +1,23 @@
 import cherrypy
 import os
+
+#workaround of cherrypy loopback check for heroku
+#http://stackoverflow.com/a/9271296
+def fake_wait_for_occupied_port(host, port): return
+cherrypy.process.servers.wait_for_occupied_port = fake_wait_for_occupied_port
+
 class HelloWorld(object):
     def index(self):
         return "Hello World!"
     index.exposed = True
+    
+    def hello(self,world='World'): #e.g. /hello/heroku/
+        return "Hello "+world
+    hello.exposed = True
 
-cherrypy.config.update({'server.socket_host': '0.0.0.0',})
-cherrypy.config.update({'server.socket_port': int(os.environ.get('PORT', '5000')),})
+cherrypy.config.update({
+    'server.socket_host': '0.0.0.0',
+    'server.socket_port': int(os.environ.get('PORT', '5000')),
+})
+
 cherrypy.quickstart(HelloWorld())
